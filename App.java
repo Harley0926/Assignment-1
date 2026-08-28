29e644f442a4d34913bd0bb01b66b444602155fb
Harley0926 <kennon_harley@students.columbusstate.edu>
2026-08-28 19:59:12 -0400

    Added scanner for user answers and an array for the questions

--- App.java
+++ App.java
@@ -1,1 +1,12 @@
+import java util.Scanner;
 
+public class App{
+  public static void main(String[] args){
+    Scanner scnr = new Scanner(System.in);
+    String[] questions = {
+      "What is the max height in Minecraft?",
+      "What is a mob that drops Ender Pearls?",
+      "What mob can be tamed using bones?"
+    };
+  }
+}
